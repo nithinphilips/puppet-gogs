00019931c840c10497d0d7ce742d28d663b2ca11
Andrea Giardini <contact@andreagiardini.com>
2015-11-05 14:20:26 +0100

    Removed quoted boolean

--- manifests/params.pp
+++ manifests/params.pp
@@ -25,8 +25,8 @@ class gogs::params {
   $db_ssl_mode     = 'disable'
   $db_data         = 'data/gogs.db'
   $app_name        = 'Gogs: Go Git Service'
-  $disable_registration = 'false'
-  $require_signin_view = 'false'
+  $disable_registration = false
+  $require_signin_view = false
   $secret_key      = 'jdkR3DBcXUDdznd'
 
   # Variables
