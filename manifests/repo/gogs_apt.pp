dce66329794ff3cf6f982b5e262332f1ab19dd7f
Andrea Giardini <contact@andreagiardini.com>
2016-02-13 12:55:01 +0100

    Fix alignment

--- manifests/repo/gogs_apt.pp
+++ manifests/repo/gogs_apt.pp
@@ -7,13 +7,13 @@ class gogs::repo::gogs_apt(
   ensure_packages('apt-transport-https')
 
   apt::source { 'deb.packager.io-gogs':
-    comment     => 'This is the Gogs package repository on packager.io',
-    location    => 'https://deb.packager.io/gh/pkgr/gogs',
-    release     => $::lsbdistcodename,
-    repos       => 'pkgr',
-    key         => {
-        id     => '6257DF9972462F57A20FFB2AB6D583CCBD33EEB8',
-        source => 'https://deb.packager.io/key',
+    comment  => 'This is the Gogs package repository on packager.io',
+    location => 'https://deb.packager.io/gh/pkgr/gogs',
+    release  => $::lsbdistcodename,
+    repos    => 'pkgr',
+    key      => {
+      id     => '6257DF9972462F57A20FFB2AB6D583CCBD33EEB8',
+      source => 'https://deb.packager.io/key',
     },
     include_src => false,
     require     => [
