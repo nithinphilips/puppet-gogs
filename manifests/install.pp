3b76630781ae9e3cc2bc6cfe8cc7d769a6f120c0
Levent Tutar <ltutar@xebia.com>
2016-05-22 11:49:20 +0200

    Remove the service and the conf files of the RPM
    We need to stop and remove the gogs service and the conf file that come
    with the RPM. Otherwise, the module will not work. See
    https://github.com/Siteminds/puppet-gogs/issues/28

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,5 +9,16 @@ class gogs::install(
 
   package { $package_name:
     ensure => $package_ensure,
+  } ->
+  # The following service is installed through the rpm and must be stopped first
+  exec { 'Stopping_gogs_via_initctl' : 
+    command => 'initctl stop gogs',
+    cwd    => '/etc/init',
+    path   => ['/sbin', '/usr/bin'],
+    onlyif => ['test -f /etc/init/gogs.conf', 'test -f /etc/init/gogs-web-1.conf', 'test -f /etc/init/gogs-web.conf'],
+  } ->
+  # The following files are installed through the rpm and must be removed
+  file { ['/etc/init/gogs.conf', '/etc/init/gogs-web-1.conf', '/etc/init/gogs-web.conf']:
+    ensure => absent,
   }
 }
