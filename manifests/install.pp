9db8719acd16f59ee1c9e0daceaa26f73d0d3f55
Levent Tutar <ltutar@xebia.com>
2016-05-27 10:32:15 +0200

    fixing rake test errors
    fixed ‘bundle exec rake test’ errors.
    extra space and alignment

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,11 +11,11 @@ class gogs::install(
     ensure => $package_ensure,
   } ->
   # The following service is installed through the rpm and must be stopped first
-  exec { 'Stopping_gogs_via_initctl' : 
+  exec { 'Stopping_gogs_via_initctl':
     command => 'initctl stop gogs',
-    cwd    => '/etc/init',
-    path   => ['/sbin', '/usr/bin'],
-    onlyif => ['test -f /etc/init/gogs.conf', 'test -f /etc/init/gogs-web-1.conf', 'test -f /etc/init/gogs-web.conf'],
+    cwd     => '/etc/init',
+    path    => ['/sbin', '/usr/bin'],
+    onlyif  => ['test -f /etc/init/gogs.conf', 'test -f /etc/init/gogs-web-1.conf', 'test -f /etc/init/gogs-web.conf'],
   } ->
   # The following files are installed through the rpm and must be removed
   file { ['/etc/init/gogs.conf', '/etc/init/gogs-web-1.conf', '/etc/init/gogs-web.conf']:
